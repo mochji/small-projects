326ba4abb9284fff295dba4e1e978a0457d1602c
mochji <owo.mochji@gmail.com>
2025-04-27 21:17:44 -0400

    nvm the previous one i made was better

--- markov.py
+++ markov.py
@@ -34,14 +34,14 @@ def train(weights, data):
 
 def choose(weights, r):
     choice = 0
+    floor  = 0
 
     for weight in weights:
-        r -= weight
-
-        if r <= 0:
+        if r >= floor and r < floor + weight:
             return choice
 
         choice += 1
+        floor  += weight
 
     return choice
 
